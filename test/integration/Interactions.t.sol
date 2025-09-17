8dbacd7ef3402efeba7a86d6901d07b47f5b9c43
Yumeng Zhang <yumengzhang@YumengdeMacBook-Air.local>
2025-09-17 19:12:26 +0800

    third-commit

--- test/integration/Interactions.t.sol
+++ test/integration/Interactions.t.sol
@@ -2,117 +2,89 @@
 
 pragma solidity ^0.8.19;
 
-import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/Script.sol";
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
-import {HelperConfig} from "../../script/HelperConfig.s.sol";
-import {Test, console} from "forge-std/Test.sol";
-import {Vm} from "forge-std/Vm.sol";
-import {StdCheats} from "forge-std/StdCheats.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
-import {CreateSubscription} from "../../script/Interactions.s.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "script/Interactions.s.sol";
 
-contract RaffleTest is StdCheats, Test {
-    /* Errors */
-    event RequestedRaffleWinner(uint256 indexed requestId);
-    event RaffleEnter(address indexed player);
-    event WinnerPicked(address indexed player);
-
+contract InteractionTest is Test {
     Raffle public raffle;
     HelperConfig public helperConfig;
+    CreateSubscription public createSubscription;
+    FundSubscription public fundSubscription;
+    AddConsumer public addConsumer;
+    VRFCoordinatorV2_5Mock public vrfCoordinatorMock;
 
-    uint256 subscriptionId;
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinator;
     bytes32 gasLane;
-    uint256 automationUpdateInterval;
-    uint256 raffleEntranceFee;
+    uint256 subscriptionId;
     uint32 callbackGasLimit;
-    address vrfCoordinatorV2_5;
+    address account;
+    address link;
 
-    address public PLAYER = makeAddr("player");
-    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+    uint256 public constant FUND_AMOUNT = 3 ether;
 
-    function setUp() external {
+    function setUp() external{
         DeployRaffle deployer = new DeployRaffle();
         (raffle, helperConfig) = deployer.deployContract();
-        vm.deal(PLAYER, STARTING_USER_BALANCE);
-
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
-        subscriptionId = config.subscriptionId;
+        entranceFee = config.entranceFee;
+        interval = config.interval;
+        vrfCoordinator = config.vrfCoordinator;
         gasLane = config.gasLane;
-        automationUpdateInterval = config.interval;
-        raffleEntranceFee = config.entranceFee;
+        subscriptionId = config.subscriptionId;
         callbackGasLimit = config.callbackGasLimit;
-        vrfCoordinatorV2_5 = config.vrfCoordinator;
+        account = config.account;
+        link = config.link;
+
+        createSubscription = new CreateSubscription();
+        fundSubscription = new FundSubscription();
+        addConsumer = new AddConsumer();
+
+        vrfCoordinatorMock = VRFCoordinatorV2_5Mock(vrfCoordinator);
     }
 
-    /////////////////////////
-    // fulfillRandomWords //
-    ////////////////////////
-
-    modifier raffleEntered() {
-        vm.prank(PLAYER);
-        raffle.enterRaffle{value: raffleEntranceFee}();
-        vm.warp(block.timestamp + automationUpdateInterval + 1);
-        vm.roll(block.number + 1);
-        _;
-    }
-
-    modifier onlyOnDeployedContracts() {
-        if (block.chainid == 31337) {
-            return;
-        }
-        try vm.activeFork() returns (uint256) {
-            return;
-        } catch {
-            _;
-        }
-    }
-
-    function testFulfillRandomWordsCanOnlyBeCalledAfterPerformUpkeep() public raffleEntered onlyOnDeployedContracts {
+    function testCreateSubscription() public {
         // Arrange
-        // Act / Assert
-        vm.expectRevert("nonexistent request");
-        // vm.mockCall could be used here...
-        VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fulfillRandomWords(0, address(raffle));
-
-        vm.expectRevert("nonexistent request");
-
-        VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fulfillRandomWords(1, address(raffle));
-    }
-
-    function testFulfillRandomWordsPicksAWinnerResetsAndSendsMoney() public raffleEntered onlyOnDeployedContracts {
-        address expectedWinner = address(1);
-
-        // Arrange
-        uint256 additionalEntrances = 3;
-        uint256 startingIndex = 1; // We have starting index be 1 so we can start with address(1) and not address(0)
-
-        for (uint256 i = startingIndex; i < startingIndex + additionalEntrances; i++) {
-            address player = address(uint160(i));
-            hoax(player, 1 ether); // deal 1 eth to the player
-            raffle.enterRaffle{value: raffleEntranceFee}();
-        }
-
-        uint256 startingTimeStamp = raffle.getLastTimeStamp();
-        uint256 startingBalance = expectedWinner.balance;
-
         // Act
-        vm.recordLogs();
-        raffle.performUpkeep(""); // emits requestId
-        Vm.Log[] memory entries = vm.getRecordedLogs();
-        bytes32 requestId = entries[1].topics[1]; // get the requestId from the logs
-
-        VRFCoordinatorV2_5Mock(vrfCoordinatorV2_5).fulfillRandomWords(uint256(requestId), address(raffle));
-
+        (uint256 subId,) = createSubscription.createSubscription(vrfCoordinator, account);
         // Assert
-        address recentWinner = raffle.getRecentWinner();
-        Raffle.RaffleState raffleState = raffle.getRaffleState();
-        uint256 winnerBalance = recentWinner.balance;
-        uint256 endingTimeStamp = raffle.getLastTimeStamp();
-        uint256 prize = raffleEntranceFee * (additionalEntrances + 1);
+        assert(subId > 0);
+    }
 
-        assert(recentWinner == expectedWinner);
-        assert(uint256(raffleState) == 0);
-        assert(winnerBalance == startingBalance + prize);
-        assert(endingTimeStamp > startingTimeStamp);
+    function testFundSubscription() public {
+        // Arrange
+        // Act
+        // Create a subscription
+        (uint256 subId,) = createSubscription.createSubscription(vrfCoordinator, account);
+        (uint96 balanceBeforeFund, , , ,) = vrfCoordinatorMock.getSubscription(subId);
+        console.log("Balance before fund: ", balanceBeforeFund);
+
+        // Fund the subscription
+        fundSubscription.fundSubscription(vrfCoordinator, subId, link, account);
+        (uint96 balanceAfterFund, , , ,) = vrfCoordinatorMock.getSubscription(subId);
+        console.log("Balance after fund: ", balanceAfterFund);
+        // Assert
+        assertEq(FUND_AMOUNT * 100, balanceAfterFund - balanceBeforeFund);
+    }
+
+    function testAddConsumer() public {
+        // Arrange
+        // Act
+        // Create a subscription
+        (uint256 subId,) = createSubscription.createSubscription(vrfCoordinator, account);
+        // Fund the subscription
+        fundSubscription.fundSubscription(vrfCoordinator, subId, link, account);
+        // Don't have tp deploy Raffle again, we already did that in the setUp function
+        addConsumer.addConsumer(address(raffle), vrfCoordinator, subId, account);
+        // Assert
+        (, , , , address[] memory consumers) = vrfCoordinatorMock.getSubscription(subId);
+        assertEq(consumers[0], address(raffle));
     }
 }
